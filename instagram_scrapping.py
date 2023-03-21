b04a445acbd529c11f4247365d60c2f220f58e3c
cvegas <cvegasavila6@gmail.com>
2023-03-21 17:00:12 +0100

    comentario para saber donde cambiar la clase de bootstrap

--- instagram_scrapping.py
+++ instagram_scrapping.py
@@ -174,7 +174,7 @@ df = pd.DataFrame(results, columns=['Cuenta', 'Fecha', 'Descripcion', 'URL', 'Fo
 
 df.index += 1
 
-html_table = df.to_html(escape=False,formatters=dict(URL=url_html_formatter, Foto=image_html_formatter)).replace('dataframe', 'table')
+html_table = df.to_html(escape=False,formatters=dict(URL=url_html_formatter, Foto=image_html_formatter)).replace('dataframe', 'table') # CLASS FOR THE BOOTSTRAP STYLESHEET
 
 html_template = '<!DOCTYPE html><head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></head><body>' + str(html_table) + '</body></html>'
 
