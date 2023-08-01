11a6d6b4903a11d92b95fe6f43223d64bb68c732
danidbarrio <daniel.barrio.aguilera@gmail.com>
2023-08-01 19:20:34 +0200

    corregida la obtencion del texto

--- instagram_scrapping.py
+++ instagram_scrapping.py
@@ -105,7 +105,7 @@ for url in data['Link'].tolist():
     publicaciones_actuales = driver.find_elements(By.TAG_NAME, 'article')
 
     # Hacer scroll hacia abajo hasta que no haya más publicaciones
-    total_posts = driver.find_element_by_css_selector('span._ac2a span')
+    total_posts = driver.find_element(By.CSS_SELECTOR, 'span._ac2a span').text
     print(total_posts)
     while str(publicaciones_actuales) < str(total_posts):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
